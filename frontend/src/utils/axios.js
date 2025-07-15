c02313afad6095db932c92c0571c9bd526c84075
Nikhil Mohanty <nikhilmohanty443@gmail.com>
2025-07-15 20:21:19 +0530

    changed baseURL

--- frontend/src/utils/axios.js
+++ frontend/src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'https://todo-app-aaua.onrender.com',
+  baseURL: 'https://todo-app-aaua.onrender.com/api',
 });
 
 API.interceptors.request.use((req) => {
